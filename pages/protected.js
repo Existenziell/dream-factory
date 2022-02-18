2b59b8731f32d2da73e2717553d7cc9b872c01ea
Christof <bauer.christof@gmail.com>
2022-02-18 15:51:23 -0500

    Mobile

--- pages/protected.js
+++ pages/protected.js
@@ -20,15 +20,15 @@ export default function Protected({ hasReadPermission }) {
       <main className="px-4 md:px-8 pb-16">
 
         <h1 className="mt-8 mb-1 text-4xl">Dream Factory Cozumel</h1>
-        <h2 className="mb-8 italic">Projects Roadmap</h2>
+        <h2 className="mb-12 italic">Projects Roadmap</h2>
         <ul className="flex flex-col items-center justify-center gap-8">
           {projects.map(p => {
             return (
-              <li key={p.title} className='w-full p-4 bg-gray-100 rounded shadow'>
-                <div className="flex items-center justify-between space-x-8 w-full">
+              <li key={p.title} className='w-full p-4 bg-gray-100 rounded shadow overflow-hidden'>
+                <div className="flex flex-col md:flex-row items-start md:items-center justify-between md:space-x-8">
                   <div>
                     <h3 className="text-4xl mb-2 w-max">{p.title}</h3>
-                    <p className="my-4">{p.desc}</p>
+                    <p className="my-4 p-3 bg-white text-gray-600">{p.desc}</p>
                     <p>
                       <span className="w-24 inline-block">URLs:</span>
                       {p.urls.map(u => (<a href={u} target='_blank' rel="noreferrer" className="inline-block mr-4">{u}</a>))}
@@ -50,7 +50,7 @@ export default function Protected({ hasReadPermission }) {
                       </p>
                     }
                   </div>
-                  <img src={`/${p.image}`} className='max-h-40' />
+                  <img src={`/${p.image}`} className='max-h-40 mt-8 md:mt-0' />
                 </div>
               </li>
             )
