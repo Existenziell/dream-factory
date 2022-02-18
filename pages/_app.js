0c3eab3151cedd943e4abbb3636bff1abedd3c17
Christof <bauer.christof@gmail.com>
2022-02-18 14:54:58 -0500

    Remove unneeded stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,17 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
-import Head from 'next/head'
 import Cookies from "universal-cookie"
 import consts from "consts"
 import App from "next/app"
 
 function DreamFactory({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+    <Component {...pageProps} />
+  )
 }
 
 DreamFactory.getInitialProps = async (appContext) => {
   const appProps = await App.getInitialProps(appContext)
-  // let appProps
   const cookies = new Cookies(appContext.ctx.req.headers.cookie)
   const password = cookies.get(consts.SiteReadCookie) ?? ""
 
